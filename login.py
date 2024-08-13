3426aa41c45a062bfdbc376d0245009fa8202db2
packshark <sb016n@att.com>
2024-08-13 10:19:20 -0700

    Update login.py

--- login.py
+++ login.py
@@ -7,6 +7,6 @@ st.write("Please log in to continue")
 # Display login form
 with st.form("login_form"):
     username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
+    password = st.text_input("Password")
     submit_button = st.form_submit_button('Submit')
 
