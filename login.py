2acbcc2f29488859d5cdbe58e086f8f710f5ef1d
packshark <sb016n@att.com>
2024-08-13 09:34:02 -0700

    initialize log in state and add reload statement

--- login.py
+++ login.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from navigation import make_sidebar
 
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
 make_sidebar()
 
 st.title("Welcome to the Packing Portal")
@@ -14,15 +17,18 @@ with st.form("login_form"):
         if username == "phone" and password == "hellokitty":
             st.session_state.logged_in = True
             st.success("Logged in successfully!")
+            st.experimental_rerun()  # Reload the page to reflect the login state
             st.switch_page("cust.py")
         elif username == "laptop" and password == "chamberofsecrets":
             st.session_state.logged_in = True
             st.success("Logged in successfully!")
               # should show filtered views, so take them to different pages and tables
+            st.experimental_rerun()  # Reload the page to reflect the login state
             st.switch_page("operations.py")
         elif username == "hehe" and password == "helloworld":
             st.session_state.logged_in = True
             st.success("Logged in successfully!")
+            st.experimental_rerun()  # Reload the page to reflect the login state
             st.switch_page("basicUser.py")
         else:
             st.error("Incorrect username or password")
