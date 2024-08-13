82c86c89d610a5f919b877d82c86e2450dfa9a28
packshark <sb016n@att.com>
2024-08-13 09:36:50 -0700

    remove the log in state

--- login.py
+++ login.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from navigation import make_sidebar
 
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
+#if "logged_in" not in st.session_state:
+ #   st.session_state.logged_in = False
 
 make_sidebar()
 
@@ -17,18 +17,20 @@ with st.form("login_form"):
         if username == "phone" and password == "hellokitty":
             st.session_state.logged_in = True
             st.success("Logged in successfully!")
-            st.experimental_rerun()  # Reload the page to reflect the login state
             st.switch_page("cust.py")
+            st.experimental_rerun()  # Reload the page to reflect the login state
         elif username == "laptop" and password == "chamberofsecrets":
             st.session_state.logged_in = True
             st.success("Logged in successfully!")
               # should show filtered views, so take them to different pages and tables
             st.experimental_rerun()  # Reload the page to reflect the login state
             st.switch_page("operations.py")
+            st.experimental_rerun()  # Reload the page to reflect the login state
         elif username == "hehe" and password == "helloworld":
             st.session_state.logged_in = True
             st.success("Logged in successfully!")
             st.experimental_rerun()  # Reload the page to reflect the login state
             st.switch_page("basicUser.py")
+            st.experimental_rerun()  # Reload the page to reflect the login state
         else:
             st.error("Incorrect username or password")
