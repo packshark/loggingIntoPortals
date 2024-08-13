78c3f5de09c23dd65b12e8a9e6d5a20e2277a9e9
packshark <sb016n@att.com>
2024-08-13 09:52:49 -0700

    Update login.py

--- login.py
+++ login.py
@@ -17,25 +17,25 @@ with st.form("login_form"):
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
     submit_button = st.form_submit_button('Submit')
-
-if submit_button:
-if username == "phone" and password == "hellokitty":
-    st.experimental_rerun()
-    st.session_state.logged_in = True
-    st.success("Logged in successfully!")
-    # Reload the page to reflect the login state        
-    st.switch_page("cust.py")  
-    st.experimental_rerun() 
-elif username == "laptop" and password == "chamberofsecrets":
-    st.experimental_rerun()
-    st.session_state.logged_in = True
-    st.success("Logged in successfully!")        
-    st.experimental_rerun()  # Reload the page to reflect the login state
-    st.switch_page("operations.py")  
-elif username == "hehe" and password == "helloworld":
-    st.session_state.logged_in = True
-    st.success("Logged in successfully!")        
-    st.experimental_rerun()  # Reload the page to reflect the login state
-    st.switch_page("basicUser.py")  
-else:        
-    st.error("Incorrect username or password")
+    
+    if submit_button:
+        if username == "phone" and password == "hellokitty":
+            st.experimental_rerun()
+            st.session_state.logged_in = True
+            st.success("Logged in successfully!")
+            # Reload the page to reflect the login state        
+            st.switch_page("cust.py")  
+            st.experimental_rerun() 
+    elif username == "laptop" and password == "chamberofsecrets":
+        st.experimental_rerun()
+        st.session_state.logged_in = True
+        st.success("Logged in successfully!")        
+        st.experimental_rerun()  # Reload the page to reflect the login state
+        st.switch_page("operations.py")  
+    elif username == "hehe" and password == "helloworld":
+        st.session_state.logged_in = True
+        st.success("Logged in successfully!")        
+        st.experimental_rerun()  # Reload the page to reflect the login state
+        st.switch_page("basicUser.py")  
+    else:        
+        st.error("Incorrect username or password")
