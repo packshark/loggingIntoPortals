1017c3f25fd2dfad5ddbc3a5f1bda8436b3a5322
packshark <sb016n@att.com>
2024-08-13 08:56:09 -0700

    remove sleep

--- login.py
+++ login.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from time import sleep
 from navigation import make_sidebar
 
 make_sidebar()
@@ -15,18 +14,15 @@ if st.button("Log in", type="primary"):
     if username == "phone" and password == "hellokitty":
         st.session_state.logged_in = True
         st.success("Logged in successfully!")
-        sleep(0.5)
         st.switch_page("cust.py")
     elif username == "laptop" and password == "chamberofsecrets":
         st.session_state.logged_in = True
         st.success("Logged in successfully!")
-        sleep(0.5)
       # should show filtered views, so take them to different pages and tables
         st.switch_page("operations.py")
     elif username == "hehe" and password == "helloworld":
         st.session_state.logged_in = True
         st.success("Logged in successfully!")
-        sleep(0.5)
         st.switch_page("basicUser.py")
     else:
         st.error("Incorrect username or password")
